1d7457e13b590d1d470aec4c6f67d023b001bc56
Dongwan Kim <zeumes12@gmail.com>
2019-03-17 22:30:20 -0400

    Added party list page, and added table schema sql

--- PartyHive/Controllers/PartiesController.cs
+++ PartyHive/Controllers/PartiesController.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PartyHive.Models;
 
 namespace PartyHive.Controllers
 {
     public class PartiesController : Controller
     {
+        private readonly PartyHiveContext _context;
+        public PartiesController(PartyHiveContext context)
+        {
+            _context = context;
+        }
+        // parties/index
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Party> allParties = _context.Party.Include(c => c.Host).ToArray();
+            return View(allParties);
         }
+        // parties/detail/
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var party = await _context.Party
+                                    .Include(c => c.Host)
+                                    .Where(x => x.Id.Equals(id))
+                                    .FirstOrDefaultAsync();
+            if(party == null)
+            {
+                return NotFound();
+            }
+
+            return View(party);
+        }
+
     }
 }
