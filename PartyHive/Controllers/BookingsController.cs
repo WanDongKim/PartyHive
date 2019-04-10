e78b2c2335a21b6e025a8ade50a12f5f25823c14
Dongwan Kim <zeumes12@gmail.com>
2019-04-10 00:47:26 -0400

    Added booking feature for only logged in customers

---
+++ PartyHive/Controllers/BookingsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PartyHive.Models;
+
+namespace PartyHive.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private readonly PartyHiveContext _context;
+        public BookingsController(PartyHiveContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Confirmation(int BookingId, int UserId, int PartyId)
+        {
+            Booking booking = _context.Booking.Include(c=>c.Party).Include(c=>c.User).Where(x => x.BookingId.Equals(BookingId)).FirstOrDefault();
+            return View(booking);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Confirmation([Bind("BookingId, UserId, PartyId, HowManyGuest")]Booking booking)
+        {
+            if (booking.UserId == 0)
+            {
+                return NotFound();
+            }
+            if (booking.PartyId == 0)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                Party party = _context.Party.Where(x => x.Id.Equals(booking.PartyId)).FirstOrDefault();
+                if(!(Convert.ToInt32(party.CurrentEnrollment) >= Convert.ToInt32(party.MaxEnrollment)))
+                {
+                    party.CurrentEnrollment = (Convert.ToInt32(party.CurrentEnrollment) + booking.HowManyGuest).ToString();
+                    if (HttpContext.Session.GetInt32("token") != null)
+                    {
+                        booking.Party = party;
+                        booking.User = _context.User.Where(x => x.Id.Equals(booking.UserId)).FirstOrDefault();
+                    }
+                    _context.Add(booking);
+                    _context.Update(party);
+
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Confirmation", "Bookings", new { booking.BookingId, booking.UserId, booking.PartyId });
+                }
+            }
+
+            return View(booking);
+        }
+    }
+}
--- PartyHive/Models/Booking.cs
+++ PartyHive/Models/Booking.cs
@@ -8,6 +8,7 @@ namespace PartyHive.Models
         public int BookingId { get; set; }
         public int UserId { get; set; }
         public int PartyId { get; set; }
+        public int? HowManyGuest { get; set; }
 
         public Party Party { get; set; }
         public User User { get; set; }
